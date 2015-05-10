3c92e93fa341ac73433ec64a6a6b377a4a4ba959
E-Lynn Yap <yapelynn@gmail.com>
2015-05-09 23:28:25 -0400

    fixed pie charts and started styling homepage

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require './calculation.rb'
 
-require 'pry'
+require 'pry-byebug'
 
 get '/' do
 	erb :index
@@ -11,10 +11,10 @@ end
 post '/' do
 	@answers = { :businesses => params[:businesses], :consumers => params[:consumers], :branding => params[:branding], :local => params[:local], :site => params[:site], :ecommerce => params[:ecommerce], :mobile => params[:mobile], :saas => params[:saas], :contentCreation => params[:contentCreation], :ltv => params[:ltv].to_i, :budget => params[:budget].to_i }
 	@@results = calculate_final_score(@answers)
-	@@budget = calculate_budget(@@results)
+	@@budget = calculate_budget(calculate_final_score(@answers), @answers[:budget])
 	redirect('/results')
 end
 
 get '/results' do
-	erb :results, :locals => {:results => @@results}
+	erb :results, :locals => { :results => @@results, :budget => @@budget }
 end
--- calculation.rb
+++ calculation.rb
@@ -1,6 +1,7 @@
 def calculate_final_score(answers)
 	final_score_hash = { :facebook => 5, :twitter => 5, :display => 5, :search => 5, :retargeting => 5, :app => 0, :local => 0, :seo => 5, :content => 5, :social => 5, :pr => 5, :direct => 5, :partners => 5, :referral => 5, :email => 5 }
 	final_score_hash = adjust_for_all_answers(answers, final_score_hash)
+	return final_score_hash
 end
 
 def adjust_for_all_answers(answers, final_score_hash)
@@ -21,189 +22,189 @@ end
 
 def adjust_for_businesses(businesses, hash)
 	if businesses == "true"
-		hash[:facebook] += 5;
-		hash[:twitter] += 3;
-		hash[:display] += 2;
-		hash[:search] += 8;
-		hash[:retargeting] += 5;
-		hash[:app] += 0;
-		hash[:local] += 0;
-		hash[:seo] += 5;
-		hash[:content] += 8;
-		hash[:social] += 3;
-		hash[:pr] += 2;
-		hash[:direct] += 15;
-		hash[:partners] += 10;
-		hash[:referral] += 0;
-		hash[:email] += 10;
+		hash[:facebook] += 5
+		hash[:twitter] += 3
+		hash[:display] += 2
+		hash[:search] += 8
+		hash[:retargeting] += 5
+		hash[:app] += 0
+		hash[:local] += 0
+		hash[:seo] += 5
+		hash[:content] += 8
+		hash[:social] += 3
+		hash[:pr] += 2
+		hash[:direct] += 15
+		hash[:partners] += 10
+		hash[:referral] += 0
+		hash[:email] += 10
 	end
 	return hash
 end
 
 def adjust_for_consumers(consumers, hash)
 	if consumers == "true"
-		hash[:facebook] += 7;
-		hash[:twitter] += 3;
-		hash[:display] += 2;
-		hash[:search] += 8;
-		hash[:retargeting] += 5;
-		hash[:app] += 0;
-		hash[:local] += 0;
-		hash[:seo] += 8;
-		hash[:content] += 8;
-		hash[:social] += 8;
-		hash[:pr] += 5;
-		hash[:direct] += 0;
-		hash[:partners] += 5;
-		hash[:referral] += 5;
-		hash[:email] += 8;
+		hash[:facebook] += 7
+		hash[:twitter] += 3
+		hash[:display] += 2
+		hash[:search] += 8
+		hash[:retargeting] += 5
+		hash[:app] += 0
+		hash[:local] += 0
+		hash[:seo] += 8
+		hash[:content] += 8
+		hash[:social] += 8
+		hash[:pr] += 5
+		hash[:direct] += 0
+		hash[:partners] += 5
+		hash[:referral] += 5
+		hash[:email] += 8
 	end
 	return hash
 end
 
 def adjust_for_branding(branding, hash)
 	if branding == "true"
-		hash[:facebook] += 0;
-		hash[:twitter] += 0;
-		hash[:display] += 5;
-		hash[:search] += 0;
-		hash[:retargeting] += 0;
-		hash[:app] += 0;
-		hash[:local] += 0;
-		hash[:seo] += 0;
-		hash[:content] += 5;
-		hash[:social] += 0;
-		hash[:pr] += 5;
-		hash[:direct] += 0;
-		hash[:partners] += 0;
-		hash[:referral] += 0;
-		hash[:email] += 0;
+		hash[:facebook] += 0
+		hash[:twitter] += 0
+		hash[:display] += 5
+		hash[:search] += 0
+		hash[:retargeting] += 0
+		hash[:app] += 0
+		hash[:local] += 0
+		hash[:seo] += 0
+		hash[:content] += 5
+		hash[:social] += 0
+		hash[:pr] += 5
+		hash[:direct] += 0
+		hash[:partners] += 0
+		hash[:referral] += 0
+		hash[:email] += 0
 	end
 	return hash
 end
 
 def adjust_for_local(local, hash)
 	if local == "true"
-		hash[:facebook] += 0;
-		hash[:twitter] += 0;
-		hash[:display] += 0;
-		hash[:search] += 0;
-		hash[:retargeting] += 0;
-		hash[:app] += 0;
-		hash[:local] += 25;
-		hash[:seo] += 0;
-		hash[:content] += 0;
-		hash[:social] += 0;
-		hash[:pr] += 0;
-		hash[:direct] += 0;
-		hash[:partners] += 0;
-		hash[:referral] += 0;
-		hash[:email] += 0;
+		hash[:facebook] += 0
+		hash[:twitter] += 0
+		hash[:display] += 0
+		hash[:search] += 0
+		hash[:retargeting] += 0
+		hash[:app] += 0
+		hash[:local] += 25
+		hash[:seo] += 0
+		hash[:content] += 0
+		hash[:social] += 0
+		hash[:pr] += 0
+		hash[:direct] += 0
+		hash[:partners] += 0
+		hash[:referral] += 0
+		hash[:email] += 0
 	end
 	return hash
 end
 
 def adjust_for_site(site, hash)
 	if site == "true"
-		hash[:facebook] += 0;
-		hash[:twitter] += 0;
-		hash[:display] += 0;
-		hash[:search] += 0;
-		hash[:retargeting] += 0;
-		hash[:app] += 0;
-		hash[:local] += 0;
-		hash[:seo] += 15;
-		hash[:content] += 0;
-		hash[:social] += 0;
-		hash[:pr] += 0;
-		hash[:direct] += 0;
-		hash[:partners] += 0;
-		hash[:referral] += 0;
-		hash[:email] += 0;
+		hash[:facebook] += 0
+		hash[:twitter] += 0
+		hash[:display] += 0
+		hash[:search] += 0
+		hash[:retargeting] += 0
+		hash[:app] += 0
+		hash[:local] += 0
+		hash[:seo] += 15
+		hash[:content] += 0
+		hash[:social] += 0
+		hash[:pr] += 0
+		hash[:direct] += 0
+		hash[:partners] += 0
+		hash[:referral] += 0
+		hash[:email] += 0
 	end
 	return hash
 end
 
 def adjust_for_ecommerce(ecommerce, hash)
 	if ecommerce == "true"
-		hash[:facebook] += 5;
-		hash[:twitter] += 0;
-		hash[:display] += 0;
-		hash[:search] += 5;
-		hash[:retargeting] += 5;
-		hash[:app] += 0;
-		hash[:local] += 0;
-		hash[:seo] += 5;
-		hash[:content] += 5;
-		hash[:social] += 5;
-		hash[:pr] += 0;
-		hash[:direct] += 0;
-		hash[:partners] += 0;
-		hash[:referral] += 5;
-		hash[:email] += 5;
+		hash[:facebook] += 5
+		hash[:twitter] += 0
+		hash[:display] += 0
+		hash[:search] += 5
+		hash[:retargeting] += 5
+		hash[:app] += 0
+		hash[:local] += 0
+		hash[:seo] += 5
+		hash[:content] += 5
+		hash[:social] += 5
+		hash[:pr] += 0
+		hash[:direct] += 0
+		hash[:partners] += 0
+		hash[:referral] += 5
+		hash[:email] += 5
 	end
 	return hash
 end
 
 def adjust_for_mobile(mobile, hash)
 	if mobile == "true"
-		hash[:facebook] += 0;
-		hash[:twitter] += 0;
-		hash[:display] += 0;
-		hash[:search] += 0;
-		hash[:retargeting] += 0;
-		hash[:app] += 50;
-		hash[:local] += 0;
-		hash[:seo] += 0;
-		hash[:content] += 0;
-		hash[:social] += 0;
-		hash[:pr] += 0;
-		hash[:direct] += 0;
-		hash[:partners] += 0;
-		hash[:referral] += 0;
-		hash[:email] += 0;
+		hash[:facebook] += 0
+		hash[:twitter] += 0
+		hash[:display] += 0
+		hash[:search] += 0
+		hash[:retargeting] += 0
+		hash[:app] += 50
+		hash[:local] += 0
+		hash[:seo] += 0
+		hash[:content] += 0
+		hash[:social] += 0
+		hash[:pr] += 0
+		hash[:direct] += 0
+		hash[:partners] += 0
+		hash[:referral] += 0
+		hash[:email] += 0
 	end
 	return hash
 end
 
 def adjust_for_saas(saas, hash)
 	if saas == "true"
-		hash[:facebook] += 5;
-		hash[:twitter] += 5;
-		hash[:display] += 0;
-		hash[:search] += 5;
-		hash[:retargeting] += 5;
-		hash[:app] += 0;
-		hash[:local] += 0;
-		hash[:seo] += 5;
-		hash[:content] += 5;
-		hash[:social] += 5;
-		hash[:pr] += 0;
-		hash[:direct] += 0;
-		hash[:partners] += 10;
-		hash[:referral] += 10;
-		hash[:email] += 10;
+		hash[:facebook] += 5
+		hash[:twitter] += 5
+		hash[:display] += 0
+		hash[:search] += 5
+		hash[:retargeting] += 5
+		hash[:app] += 0
+		hash[:local] += 0
+		hash[:seo] += 5
+		hash[:content] += 5
+		hash[:social] += 5
+		hash[:pr] += 0
+		hash[:direct] += 0
+		hash[:partners] += 10
+		hash[:referral] += 10
+		hash[:email] += 10
 	end
 	return hash
 end
 
 def adjust_for_contentCreation(contentCreation, hash)
 	if contentCreation == "true"
-		hash[:facebook] += 0;
-		hash[:twitter] += 0;
-		hash[:display] += 0;
-		hash[:search] += 0;
-		hash[:retargeting] += 0;
-		hash[:app] += 0;
-		hash[:local] += 0;
-		hash[:seo] += 10;
-		hash[:content] += 0;
-		hash[:social] += 10;
-		hash[:pr] += 0;
-		hash[:direct] += 0;
-		hash[:partners] += 0;
-		hash[:referral] += 0;
-		hash[:email] += 10;
+		hash[:facebook] += 0
+		hash[:twitter] += 0
+		hash[:display] += 0
+		hash[:search] += 0
+		hash[:retargeting] += 0
+		hash[:app] += 0
+		hash[:local] += 0
+		hash[:seo] += 10
+		hash[:content] += 0
+		hash[:social] += 10
+		hash[:pr] += 0
+		hash[:direct] += 0
+		hash[:partners] += 0
+		hash[:referral] += 0
+		hash[:email] += 10
 	end
 	return hash
 end
@@ -246,11 +247,11 @@ def calculate_percentages(hash)
 	return hash
 end
 
-def calculate_budget(hash)
+def calculate_budget(hash, budget)
 	[:seo, :content, :social, :pr, :direct, :partners, :referral, :email].each { |non_ad_channel| hash.delete(non_ad_channel) }
 	total_budget = hash.values.inject { |a,b| a + b }
 	hash.each do | channel, score |
-		hash[channel] = score / total_budget
+		hash[channel] = (score / total_budget * budget).round
 	end
 	return hash
 end
